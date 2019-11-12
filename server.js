c97a1e9e45a53a58c1b45d194b7865f051f21e4b
Ruben <ruben.zagon@gmail.com>
2019-11-12 18:04:33 +0000

    Añadida la función de contar vocales

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-let phrase = process.argv[2];
+/**
+ * Cuenta palabras
+ */
+const phrase = process.argv[2];
+const letter = "a";
 
-let countWords = (phrase) => {
+const countWords = (phrase) => {
     return phrase.split(" ").length;
 }
 
-console.log(countWords(phrase));
+const findA = (phrase, letter) => {
+
+    letterCount = 0
+
+    for (var position = 0; position < phrase.length; position++) {
+        if (phrase.charAt(position) === letter) {
+            letterCount += 1;
+        }
+    }
+    return letterCount;
+}
+
+
+console.log(`El número de palabras: ${countWords(phrase)}`);
+console.log(`"a" encontradas: ${findA(phrase, letter)}`);
