e658302c7e6a9931ab915aa46bdc5bfd0617d54b
Ruben <ruben.zagon@gmail.com>
2019-11-13 16:34:57 +0000

    Probando a leer ficheros json

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require ('express');
 const app = express();
 
+const usuarios = [{name: 'Pepe'}, {name: 'Juan'}];
 
 app.get ('/', (req,res) =>{
-	console.log('Request:', req);
-	res.send('Hello World!')
+	res.json(usuarios);
+	
 });
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
