8965908733dc32597732b3d9a516231c97754c3d
Ruben <ruben.zagon@gmail.com>
2019-11-13 16:30:31 +0000

    Vemos el request

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+
+
+app.get ('/', (req,res) =>{
+	console.log('Request:', req);
+	res.send('Hello World!')
+});
+
+app.listen(3000, () => console.log('Ready on port 3000!'));
