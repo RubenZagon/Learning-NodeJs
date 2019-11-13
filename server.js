5d3caabb0aa2387656e7b2f9ba33670a698a5d3f
Ruben <ruben.zagon@gmail.com>
2019-11-13 17:30:33 +0000

    Dado personalizable con el número de caras que le metas por parámetros

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-let rollDice = () => {
-	return Math.ceil(Math.random() * 6);
+let rollDice = (numberOfFaces) => {
+	return Math.ceil(Math.random() * parseInt(numberOfFaces));
+}
+
+const getNumber = (number) => {
+	const regExp = /^\d+$/;
+	return number.match(regExp);
 }
 
 app.get('/dice', (req, res) => {
@@ -10,5 +15,22 @@ app.get('/dice', (req, res) => {
 	res.json([{ ResultOfRollDice: dice }]);
 
 });
+
+app.get('/dice/:numberOfFaces', (req, res) => {
+
+	const numbFaces = getNumber(req.params.numberOfFaces);
+
+	if (!numbFaces) {
+		res.json({ error: "Introduzca un número válido" });
+	}
+
+	resultOfRollDice = rollDice(numbFaces);
+
+	res.json([{ result: resultOfRollDice }]);
+
+
+
+});
+
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
